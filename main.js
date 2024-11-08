b947f357808513bc8906d4669dc20f05daf00650
farahftouhi <farahftouhi68@gmail.com>
2024-11-08 16:02:29 +0100

    project is done

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let board=document.querySelector(".board")
+for(let i=0;i<8;i++){
+    for(let j=0;j<8;j++){
+        let box=document.createElement("div");
+        if((i+j)%2){
+            box.classList.add("bleuBox")
+        }
+        else{
+            box.classList.add("whiteBoxe") 
+        }
+        board.appendChild(box)
+        let square=document.createElement("div")
+        if((i+j)%2===0 && i>4){
+            square.classList.add("p-black")
+        }
+        else if((i+j)%2===1 && i<3){
+            square.classList.add("p-white")
+        }
+        box.appendChild(square)
+
+    }
+    
+}
